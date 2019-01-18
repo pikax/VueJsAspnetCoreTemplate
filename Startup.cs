326085899ef02eb436e817e900910184f0716076
Carlos Rodrigues <carlos.rodrigues@accountscore.com>
2019-01-18 15:15:21 +0000

    add files

---
+++ Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VueJsAspnetCore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSpaStaticFiles(x =>
+            {
+                x.RootPath = "dist"; // set static files to dist
+      });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSpaStaticFiles(new StaticFileOptions()
+            
+            {
+#if !DEBUG
+      OnPrepareResponse = ctx =>
+        {
+          // https://developers.google.com/web/fundamentals/performance/webpack/use-long-term-caching
+          ctx.Context.Response.Headers[HeaderNames.CacheControl] = "max-age=" + 31536000;
+        }  
+#endif
+
+            });
+            app.UseSpa(x =>
+            {
+                x.Options.DefaultPage = "/index.html";
+            });
+        }
+    }
+}
---
+++ dist/precache-manifest.25d1e48d2ca2f7cb0e3d77e91b99872a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e48352d2c396b5d19217",
+    "url": "/js/chunk-vendors.da0ee967.js"
+  },
+  {
+    "revision": "fe030b22fe41b9ec5f8a",
+    "url": "/js/app.ef653a21.js"
+  },
+  {
+    "revision": "6941b5669782b8fb851563bcd5f577cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fe030b22fe41b9ec5f8a",
+    "url": "/css/app.03044f30.css"
+  }
+];
